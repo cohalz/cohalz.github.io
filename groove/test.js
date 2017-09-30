34679e70be63add0310effa196a0da8ee07eec3b
cohalz <cohalzweb@gmail.com>
2017-09-30 16:16:31 +0900

    add

--- groove/test.js
+++ groove/test.js
@@ -1,1 +1,22 @@
-alert("hoge");
+var xmlHttp=new XMLHttpRequest();
+var player_name=prompt('調べたいプレイヤーネームを正確に入力してください', '');
+var month=prompt('調べたい月を入力してください\n(例:2017年3月を調べたい場合は201703と入力)', '');
+var index=0;
+var date = new Date();
+var now = date.getFullYear() + "" + ("0"+(date.getMonth()+1)).slice(-2);
+if(month.length == 6 && 201608 <= month && month <= now){
+var flag=false;
+while(!flag&&index<10){
+  xmlHttp.open("GET","https://mypage.groovecoaster.jp/sp/json/monthly_ranking.php?id="+month+"&page="+index,false); 
+  xmlHttp.send(null);
+  var data=JSON.parse(xmlHttp.responseText);
+  for(var i=0;i<data.rank.length;i++){
+    if(data.rank[i].player_name===player_name){
+      alert(player_name+"さんの"+month+"の月間ランキングは"+data.rank[i].rank+"位です");
+      flag=true;
+      break;
+    }
+  }
+  index++;
+}
+}
---
+++ index.md
@@ -0,0 +1,23 @@
+
+# cohalz
+
+## Contact
+- Twitter [@cohalz](https://twitter.com/cohalz)
+- GitHub [cohalz](https://github.com/cohalz)
+- Blog [cohаlz.hatеnablоg.сom](http://cohalz.hatenablog.com)
+
+# Construction
+- cohalzの過去の発言からマルコフ連鎖でツイートするbot (@co_hyn)[https://twitter.com/co_hyn]
+- フォローしている人でマルコフ連鎖するbot (@MOBP_)[https://twitter.com/MOBP_]
+- グルコス関連ブックマークレット
+
+## Conversation
+[Speaker Deck](https://speakerdeck.com/cohalz)
+
+
+## Contribute
+(PMOB)[https://github.com/PMOB]
+
+## Corporation
+- 芸者東京エンターテインメント インターン(2015/04 ~)
+- はてなインターン2017
